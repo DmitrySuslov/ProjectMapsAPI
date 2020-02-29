a6e8bcede08039dd5678fefaa9473252fba11859
Димка <mityka2004@yandex.ru>
2020-02-29 21:03:15 +0300

    Не работают метки

--- ProjectMapAPI.py
+++ ProjectMapAPI.py
@@ -1,26 +1,19 @@
 import os
 import sys
 import requests
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
 from PyQt5.QtGui import QPixmap
 from PyQt5.QtCore import Qt
+from PyQt5 import uic
 
 
 SCREEN_SIZE = [600, 450]
 
 
-class ProgectMapAPI(QWidget):
+class ProgectMapAPI(QMainWindow):
     def __init__(self):
-        self.geocoder_request = "http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba" \
-                                "-98533de7710b&geocode=Москва&format=json"
-        self.response = requests.get(self.geocoder_request).json()
-        self.response_params = {
-            'll': ','.join(self.response["response"]["GeoObjectCollection"]["featureMember"][0]
-                           ["GeoObject"]["Point"]["pos"].split()),
-            'z': '3',
-            'l': 'map'}
-        self.map_file = "map.png"
         super().__init__()
+        uic.loadUi('UIProjectMapsAPI.ui', self)
         self.initUI()
 
     def keyPressEvent(self, event):
@@ -56,32 +49,48 @@ class ProgectMapAPI(QWidget):
             coords[0], coords[1] = float(coords[0]), float(coords[1])
             coords[0] -= 2 * (1 / z)
             self.response_params['ll'] = ','.join(list(map(str, coords)))
-        print(self.response_params)
         self.getImage()
         self.setImage()
 
 
     def getImage(self):
+        self.geocoder_request = "http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba" \
+                                f"-98533de7710b&geocode={self.object}&format=json"
+        self.response = requests.get(self.geocoder_request).json()
+        self.coords = self.response["response"]["GeoObjectCollection"]["featureMember"][0]["GeoObject"]["Point"]["pos"]
+        print(self.coords)
+        self.response_params = {
+            'll': ','.join(self.response["response"]["GeoObjectCollection"]["featureMember"][0]
+                           ["GeoObject"]["Point"]["pos"].split()),
+            'z': '3',
+            'l': 'map',
+            'pt': ",".join([str(self.coords[0]), str(self.coords[1]), "pm2dom"])}
+        print(self.response_params['pt'])
+
         map_request = f"http://static-maps.yandex.ru/1.x/?ll={self.response_params['ll']}&" \
-                      f"z={self.response_params['z']}&l={self.response_params['l']}"
+                      f"z={self.response_params['z']}&l={self.response_params['l']}&pt={self.response_params['pt']}"
+
         self.map_response = requests.get(map_request)
 
-        # Запишем полученное изображение в файл.
         if self.map_response:
             with open(self.map_file, "wb") as file:
                 file.write(self.map_response.content)
 
     def setImage(self):
         self.pixmap = QPixmap(self.map_file)
-        self.image.setPixmap(self.pixmap)
+        self.map_label.setPixmap(self.pixmap)
+
+    def searching(self):
+        print(1)
+        self.object = self.address.text()
+        self.getImage()
+        self.setImage()
 
 
     def initUI(self):
-        self.setGeometry(100, 100, *SCREEN_SIZE)
-        self.setWindowTitle('ProjectMapsAPI')
-        self.image = QLabel(self)
-        self.image.move(0, 0)
-        self.image.resize(600, 450)
+        self.search.clicked.connect(self.searching)
+        self.object = 'Москва'
+        self.map_file = "map.png"
         self.getImage()
         self.setImage()
 
