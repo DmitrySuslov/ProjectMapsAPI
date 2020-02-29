1af468044069944432d19d23618ef60696f7c4f1
Димка <mityka2004@yandex.ru>
2020-02-29 18:21:59 +0300

    Добавлена обработка клавиш PgUp и PgDown, по нажатию на которые необходимо увеличивать и уменьшать масштаб отображения карты.

--- ProjectMapAPI.py
+++ ProjectMapAPI.py
@@ -3,6 +3,8 @@ import sys
 import requests
 from PyQt5.QtWidgets import QApplication, QWidget, QLabel
 from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
+
 
 SCREEN_SIZE = [600, 450]
 
@@ -10,37 +12,51 @@ SCREEN_SIZE = [600, 450]
 class ProgectMapAPI(QWidget):
     def __init__(self):
         self.geocoder_request = "http://geocode-maps.yandex.ru/1.x/?apikey=40d1649f-0493-4b70-98ba" \
-                                "-98533de7710b&geocode=Австралия&format=json"
+                                "-98533de7710b&geocode=Москва&format=json"
         self.response = requests.get(self.geocoder_request).json()
         self.response_params = {
             'll': ','.join(self.response["response"]["GeoObjectCollection"]["featureMember"][0]
                            ["GeoObject"]["Point"]["pos"].split()),
             'z': '3',
-            'l': 'sat'}
+            'l': 'map'}
+        self.map_file = "map.png"
         super().__init__()
-        self.getImage()
         self.initUI()
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageUp:
+            if self.map_response and int(self.response_params['z']) < 17:
+                self.response_params['z'] = int(self.response_params['z']) + 1
+        if event.key() == Qt.Key_PageDown:
+            if int(self.response_params['z']) > 2:
+                self.response_params['z'] = int(self.response_params['z']) - 1
+        self.getImage()
+        self.setImage()
+
 
     def getImage(self):
         map_request = f"http://static-maps.yandex.ru/1.x/?ll={self.response_params['ll']}&" \
                       f"z={self.response_params['z']}&l={self.response_params['l']}"
-        response = requests.get(map_request)
+        self.map_response = requests.get(map_request)
 
         # Запишем полученное изображение в файл.
-        self.map_file = "map.png"
-        with open(self.map_file, "wb") as file:
-            file.write(response.content)
+        if self.map_response:
+            with open(self.map_file, "wb") as file:
+                file.write(self.map_response.content)
+
+    def setImage(self):
+        self.pixmap = QPixmap(self.map_file)
+        self.image.setPixmap(self.pixmap)
+
 
     def initUI(self):
         self.setGeometry(100, 100, *SCREEN_SIZE)
         self.setWindowTitle('ProjectMapsAPI')
-
-        ## Изображение
-        self.pixmap = QPixmap(self.map_file)
         self.image = QLabel(self)
         self.image.move(0, 0)
         self.image.resize(600, 450)
-        self.image.setPixmap(self.pixmap)
+        self.getImage()
+        self.setImage()
 
 
 if __name__ == '__main__':
